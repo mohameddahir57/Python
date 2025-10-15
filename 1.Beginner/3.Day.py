b2069830ba0f73b90422dc64cd74344abfa3d26e
mohameddahir57 <mohameddahirosman57@gmail.com>
2025-10-15 11:37:15 +0300

    Add files via upload

---
+++ 1.Beginner/3.Day.py
@@ -0,0 +1,38 @@
+#Day 3:Basic Syntax & Variables (Variables, data types (int, float, str, bool), naming rules, and simple printing)
+
+#Variables
+my_name = "Mohamed Dahir"
+my_age = 22
+my_height = 1.85
+is_student = True
+
+#Printing
+print("Name:", my_name)
+print("Age:", my_age)
+print("Height:", my_height)
+print("Is Student:", is_student)
+
+#Formatted string
+print(f"{my_name} is {my_age} years old, {my_height}cm tall, and student status is {is_student}.") 
+
+#Using type() to check data types
+print("Data Types:")
+print("my_name:", type(my_name))
+print("my_age:", type(my_age))
+print("my_height:", type(my_height))
+print("is_student:", type(is_student))
+
+#Naming rules
+# Valid variable names
+valid_name1 = "valid"
+valid_name_2 = 123
+_validName3 = True
+validName4 = 45.67
+validName5 = "also valid"
+
+#simple printing
+print("Valid Variable Names:")
+print(valid_name1, valid_name_2, _validName3, validName4, validName5)
+
+
+
