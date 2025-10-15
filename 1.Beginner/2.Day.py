f0e4788fb50a1c23fa9c0bde2d2901fdc105c3bf
mohameddahir57 <mohameddahirosman57@gmail.com>
2025-10-15 11:10:36 +0300

    Add files via upload

---
+++ 1.Beginner/2.Day.py
@@ -0,0 +1,45 @@
+#Installing Python & Setting Up Environment
+
+#Install Python, add to PATH, learn to use VS Code/Jupyter, create a virtual environment, install packages with pip.
+
+#Install Python
+#https://www.python.org/downloads/
+#Make sure to check the box that says "Add Python to PATH" during installation.
+# Watch the video for detailed instructions.
+# https://www.youtube.com/watch?v=YYXdXT2l-Gg 
+
+
+#add to PATH
+#https://datatofish.com/add-python-to-windows-path/
+#https://www.architectryan.com/2018/03/17/add-to-the-path-on-windows-10/
+
+#Learn to use VS Code
+#https://code.visualstudio.com/docs/python/python-tutorial
+#https://www.youtube.com/watch?v=VqCgcpAypFQ    
+
+
+
+
+#I need to announce some courses you can watch if you are beginner to Python course
+#https://www.youtube.com/watch?v=FLCVd8tJA2U&t=444s --- Somalia
+#https://www.youtube.com/watch?v=K5KVEU3aaeQ&t=1965s --- English
+#https://www.youtube.com/watch?v=s2MAVjZpTlw&t=79s --- Somalia
+
+
+# Some websites you can learn Python And practice
+#https://www.w3schools.com/python/
+#https://www.learnpython.org/
+#https://www.hackerrank.com/domains/tutorials/10-days-of-python
+#https://www.codecademy.com/learn/learn-python-3
+#https://www.programiz.com/python-programming
+#https://www.sololearn.com/Course/Python/
+
+
+
+
+
+
+
+
+
+
